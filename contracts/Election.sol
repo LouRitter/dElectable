115d8c6e808c6b2c938b53979408443e3e5b11ba
louritter <louritter1@gmail.com>
2022-01-22 01:21:48 -0500

    voting function: :

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,20 +8,41 @@ contract Election {
         uint voteCount;
     }
 
-    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
 
+    mapping(uint => Candidate) public candidates;
 
     // Store Candidates Count
     uint public candidatesCount;
-
-    //create candidates
-    function addCandidate (string memory _name) private {
-        candidatesCount ++;
-        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
-    }
     // Constructor
     constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");    
     }
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+       emit votedEvent(_candidateId);
+    }
+    //create candidates
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
 }
