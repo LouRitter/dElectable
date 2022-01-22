c9a43b5577091e67a5a657ce604d6fd3046e7150
louritter <louritter1@gmail.com>
2022-01-21 23:49:10 -0500

    addCandidate function and testing

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,10 +2,26 @@ pragma solidity >=0.4.22;
 
 contract Election {
     // Read/write candidate
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
+    mapping(uint => Candidate) public candidates;
+
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    //create candidates
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
     // Constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");    
     }
 }
